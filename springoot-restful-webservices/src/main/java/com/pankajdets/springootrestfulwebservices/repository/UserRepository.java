76cdfcd6b09310fba4cf44762235c82a6bec46f2
Pankaj Kumar Ray <42623098+pankajdets@users.noreply.github.com>
2023-04-22 18:58:21 +0530

    Delete springoot-restful-webservices directory

--- springoot-restful-webservices/src/main/java/com/pankajdets/springootrestfulwebservices/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pankajdets.springootrestfulwebservices.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pankajdets.springootrestfulwebservices.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
--- springoot-restful-webservices/src/main/java/com/pankajdets/springootrestfulwebservices/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.pankajdets.springootrestfulwebservices.service;
-
-import com.pankajdets.springootrestfulwebservices.model.User;
-
-public interface UserService {
-    User createUser(User user);
-    
-}
